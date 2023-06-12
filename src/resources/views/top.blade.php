b45cc53d1a307c2aba796511de1b9e963eb2b4ab
golfhiro <tamabolt24@gmail.com>
2023-06-12 21:09:41 +0900

    トップページの作成

---
+++ src/resources/views/top.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <header>
+        <h1>Company Site</h1>
+        <nav>
+            <a href="/">トップ</a>
+        </nav>
+    </header>
+    <main>
+        <h1>これはトップページです</h1>
+    </main>
+    <footer>
+        &copy; 2023 Laravel APP
+    </footer>
+</body>
+
+</html>
