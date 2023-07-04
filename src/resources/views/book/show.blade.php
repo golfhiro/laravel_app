016d56fd6d202d40a480daf47b15a7cc85e347d6
golfhiro <tamabolt24@gmail.com>
2023-07-04 13:45:35 +0900

    [update] タグの表示

--- src/resources/views/book/show.blade.php
+++ src/resources/views/book/show.blade.php
@@ -1,21 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            詳細画面
-        </h2>
-        <x-message :message="session('message')" />
+        <div class="flex items-center justify-between">
+            <div>
+                <h2 class=" font-semibold text-xl text-gray-800 leading-tight">
+                    詳細画面</h2>
+            </div>
+            <div class="mx-auto message">
+                <x-message :message="session('message')" />
+            </div>
+        </div>
     </x-slot>
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="mx-4 sm:p-8">
             <div class="px-10 mt-4">
                 <div class="bg-white rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
-                    <div class="mt-4">
-                        <h1 class="text-lg text-gray-700 font-semibold">
-                            {{ $book->title }}
-                        </h1>
-                        <hr class="w-full border-gray-300 my-4">
-                    </div>
                     @if ($book->user_id === $user->id)
                     <div class="flex justify-end mt-4">
                         <a href="{{ route('book.edit', $book) }}">
@@ -28,6 +27,20 @@
                         </form>
                     </div>
                     @endif
+
+                    <div class="mt-4">
+                        <h1 class="text-lg text-gray-700 font-semibold">
+                            {{ $book->title }}
+                        </h1>
+                        <hr class="w-full border-gray-300 my-4">
+                    </div>
+
+                    <div class="text-right">
+                        @foreach($book->technology_tags as $technologyTag)
+                        <a href="{{ route('book.index.technology_tag', $technologyTag->name) }}" class="inline-block bg-violet-600 text-white text-sm font-bold px-2 py-1 rounded-full">{{ $technologyTag->name }}</a>
+                        @endforeach
+                    </div>
+
                     @if($book->image)
                     <img src="{{ asset('storage/images/'.$book->image) }}" class="mx-auto my-4" style="height:300px;">
                     @endif
